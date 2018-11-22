7c0adad40494393e9cb6e86057bab4dab25f3309
Dev HS <haidy_sedky@hotmail.com>
2018-11-21 18:11:32 -0800

    adjust assignment 14

--- Assignment 14/Instructions/StarterCode/static/js/app.js
+++ Assignment 14/Instructions/StarterCode/static/js/app.js
@@ -31,12 +31,52 @@ tableData.forEach( function(data) {
 	})
 });
 
-var date_input = d3.select("#datetime");
-//console.log(date_input);
-var date_inputed = date_input.text();
+
+//tableData.forEach(function(element, index) {
+//	console.log(element.datetime)
+//})
+//;
+
+var date_input = d3.select(".form-control");
 
 date_input.on ("change", function() {
 
-	tableData.filter( tableData.date == date_inputed)
+	d3.event.preventDefault();
+
+//filter the dataset based on input from user
+
+	var date_inputed = date_input.property("value");
+
+	console.log(date_inputed);
+
+	var tableFiltered = tableData.filter(data => data.datetime === date_inputed)
+
+	//d3.event.preventDefault();
+	
+//delete the tables data
+	//var table = document.getElementById("#ufo-table");
+	//console.log(table)
+	
+	//for(var i = table.rows.length - 1; i > 0; i--)
+	//{
+    //table.deleteRow(i);
+	//};
+
+	d3.event.preventDefault();
+
+//insert the tables filtered data
+	table_body = d3.select("#ufo-table").append("tbody")
+
+	tableFiltered.forEach( function(data) {
+	var table_row = table_body.append("tr")
+
+	Object.entries(data).forEach(([key, value]) => {
+		//console.log(`key : ${key}, value : ${value}`);
+		table_row.append("td").text(value)
+
+	})
+});
 })
 ;
+
+
